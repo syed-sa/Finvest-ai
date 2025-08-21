54807f4f79af94b9069dd940fb517a88807f3e8f
LucasVianaa <lucas.viana.rs@gmail.com>
2025-08-21 12:09:49 -0300

    ci: fix lint

--- ciex-backend/src/api/routes.py
+++ ciex-backend/src/api/routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from src.api.v1 import health
 
+
 # To include more routes go as
 # from src.api.v1 import <module_name>
 # then at the end of file api_router.include_router(<module_name>.router)
