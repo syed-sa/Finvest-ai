b8cc8f71e866a14bad017c12780c1cfa36a0d5e1
GabrielVGS <gabrielviana3662@gmail.com>
2025-09-01 18:37:11 -0300

    docs: add example route

--- ciex-backend/src/api/v1/health.py
+++ ciex-backend/src/api/v1/health.py
@@ -1,7 +1,6 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse, Response
 
-
 router = APIRouter()
 
 
@@ -10,3 +9,13 @@ def pong() -> Response:
     # some async operation could happen here
     # example: `data = await get_all_datas()`
     return JSONResponse({"ping": "pong!"})
+
+
+# # Example route
+# @router.get("/users", response_model=IGetResponseBase[list[UserRead]], tags=["users"])
+# async def get_users(db=Depends(get_db)):
+#     user_repo = UserRepository(db)
+
+#     users = await user_repo.all()
+
+#     return IGetResponseBase[list[User]](data=users)
